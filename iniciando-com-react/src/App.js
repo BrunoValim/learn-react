d52a77a0c7b692976e80d9df87dc704ec860a271
BrunoValim <cson_musgo@hotmail.com>
2023-08-18 12:02:15 -0300

    b

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>LALALALAL</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = { nome: 'Bruno Valim' }
+
+  render() {
+    return (
+      <h1>Hello {this.state.nome}</h1>
+    )
+  }
 }
 
+// function ComponenteFuncional() {
+//   return (
+//     <div>
+//       <h1>asass {this.state.nome}</h1>
+//     </div>
+//   );
+// }
 export default App;
